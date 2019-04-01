bed7c5f418313d13e20e09bd33c2e555656d6c66
mgoldchild <mgoldchild@gmail.com>
2019-04-01 14:49:48 +0900

    Iroiro

---
+++ research/struct2depth/_inference.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+input_dir="/home/mike/apps/models/research/struct2depth/_inference_in"
+output_dir="/home/mike/apps/models/research/struct2depth/_inference_out"
+# model_checkpoint="/home/mike/apps/models/research/struct2depth/ckpt/model-199160"
+# model_checkpoint="/home/mike/Downloads/model-96"
+
+LANG=en_US.UTF-8 python inference.py \
+    --logtostderr \
+    --file_extension png \
+    --depth \
+    --egomotion true \
+    --input_dir $input_dir \
+    --output_dir $output_dir
+    # --model_ckpt $model_checkpoint
