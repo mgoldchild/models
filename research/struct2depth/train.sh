89ebc28337803dc4f4908b17ee55ef9f559f593b
mgoldchild <mgoldchild@gmail.com>
2019-04-10 15:48:55 +0900

    Add data

--- research/struct2depth/train.sh
+++ research/struct2depth/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ckpt_dir="/home/mike/apps/models/research/struct2depth/training_out" # checkpoing
-data_dir="/home/mike/apps/models/research/struct2depth/training_in" # Set for Cityscapes
+data_dir="/home/mike/apps/models/research/struct2depth/preprocessing_out" # Set for Cityscapes
 # data_dir="/home/mike/apps/models/research/struct2depth/out_dir" # Set for Cityscapes
 # imagenet_ckpt="/home/mike/apps/models/research/struct2depth/ckpt/model-199160"
 # imagenet_ckpt="/home/mike/apps/models/research/struct2depth/paper_ckpt/model-199160"
