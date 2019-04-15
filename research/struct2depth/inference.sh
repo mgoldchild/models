a382c5ec3afa7853e8a3164986eb5980ff8f77a3
mgoldchild <mgoldchild@gmail.com>
2019-04-15 17:59:27 +0900

    Test for my images and kitti calibration for notebook instance

--- research/struct2depth/inference.sh
+++ research/struct2depth/inference.sh
@@ -5,7 +5,8 @@ output_dir="/home/mike/apps/models/research/struct2depth/inference_out"
 # model_checkpoint="/home/mike/Downloads/model-96"
 # model_checkpoint="/home/mike/Downloads/model-9968"
 # model_checkpoint="/home/mike/Downloads/model-4424"
-model_checkpoint="/home/mike/Downloads/model-2264"
+# model_checkpoint="/home/mike/Downloads/model-2264"
+model_checkpoint="/home/mike/apps/models/research/struct2depth/training_out/model-204"
 
 python inference.py \
     --logtostderr \
